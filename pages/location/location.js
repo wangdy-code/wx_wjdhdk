fa119bc3beba996eb6a7c2c5193c99cf500baa40
unknown <unknown@example.com>
2019-11-14 16:03:12 +0800

    Initial Commit

---
+++ pages/location/location.js
@@ -0,0 +1,55 @@
+var bmap = require('../../li b s/bmap-wx.min.js');
+var wxMarkerData = [];
+Page({
+  data: {
+    markers: [],
+    latitude: '',
+    longitude: '',
+    rgcData: {}
+  },
+  makertap: function (e) {
+    var that = this;
+    var id = e.markerId;
+    that.showSearchInfo(wxMarkerData, id);
+  },
+  onLoad: function () {
+    var that = this;
+    var BMap = new bmap.BMapWX({
+      ak: 'nUjcj9EfmqcL6Khzm7eSq9odMpBeDyYY'
+    });
+    var fail = function (data) {
+      console.log(data)
+    };
+    var success = function (data) {
+      wxMarkerData = data.wxMarkerData;
+      that.setData({
+        markers: wxMarkerData
+      });
+      that.setData({
+        latitude: wxMarkerData[0].latitude
+      });
+      that.setData({
+        longitude: wxMarkerData[0].longitude
+      });
+    }
+    BMap.regeocoding({
+      fail: fail,
+      success: success,
+      iconPath: '../../img/marker_red.png',
+      iconTapPath: '../../img/marker_red.png'
+    });
+    
+  },
+  showSearchInfo: function (data, i) {
+    var that = this;
+    that.setData({
+      rgcData: {
+        address: '地址：' + data[i].address + '\n',
+        desc: '描述：' + data[i].desc + '\n',
+        business: '商圈：' + data[i].business
+      }
+    });
+  }
+  
+
+})
